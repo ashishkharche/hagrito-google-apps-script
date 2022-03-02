41d0e85cd0b41c83dc9914f7f39dfd83c117de9c
ashishkharche <thekharche@gmail.com>
2022-03-02 12:35:04 +0000

    Add background color and text to cells

---
+++ code/sheets/add-background-color-and-text-to-cells/Code.js
@@ -0,0 +1,23 @@
+function addColors2(){
+    const id = '1looDMwg_ztAb2tiuRx6Xk3MXFtQ4yLc1vumVbiSnzu0';
+    const ss = SpreadsheetApp.openById(id);
+    const sheet = ss.getSheets()[0];
+    let backColor = 'red';
+    let mySize = 10;
+    for(let rows = 1;rows<51;rows++){
+      for(let cols=1;cols<11;cols++){
+          let total = rows + cols;
+          if((total%2)==0){
+            backColor = 'red';
+          }else{
+            backColor = 'pink';
+          }
+          let range = sheet.getRange(rows,cols);
+          range.setBackground(backColor);
+          range.setFontColor('white');
+          range.setFontSize(mySize+cols);
+          range.setValue(total);
+      }
+    }
+  }
+  
---
+++ code/sheets/add-background-color-and-text-to-cells/README.md
@@ -0,0 +1,5 @@
+# Add background color and text to cells
+
+Similar to the add background color to cells code but here it uses `rows, cols` instead of `charCodeAt` logic.
+
+It also increases fontSize as we go further in right direction.
